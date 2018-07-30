9658da5121d22bbf3c34df9e71d5c35dd91eac07
Jack Cambou <jcambou@gmail.com>
2018-07-30 08:57:45 -0700

    Built Static Pages

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -38,7 +38,7 @@ class Home extends React.Component {
                         <div key={dog._id}>
                             <h3> {dog.title} </h3>
                             <Link to ={`/dogs/${dog._id}`}>  <img src={dog.featuredImageUrl} /> </Link>
-                            <h5> {dog.tags}</h5>
+                            <h5> Dog Tags: {dog.tags}</h5>
                             <hr />
                         </div>
                     )
